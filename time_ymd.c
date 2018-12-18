70876dcf596f79c6c6c112cf7a564ef5fdf4effc
dheeraj2601 <dheeraj2601@gmail.com>
2018-12-18 12:32:46 +0530

    Create time_ymd.c

---
+++ time_ymd.c
@@ -0,0 +1,20 @@
+
+//How to print time in format: 2009‐08‐10 18:17:54.811
+
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    time_t timer;
+    char buffer[26];
+    struct tm* tm_info;
+
+    time(&timer);
+    tm_info = localtime(&timer);
+
+    strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+    puts(buffer);
+
+    return 0;
+}
